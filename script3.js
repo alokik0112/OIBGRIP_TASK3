09f46805f4082306042d544237d055625cc66ed8
Alokik kumar <78497754+alokik0112@users.noreply.github.com>
2023-02-24 12:59:08 +0530

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,23 @@
+const celsiusElem = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+
+window.addEventListener("load", () => {degree.value = ""; celsiusElem.innerHTML = "";});
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convert();
+});
+
+function convert() {
+  let inputValue = degree.value;
+
+if (tempType.value === "celsius") {
+    const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+    celsiusElem.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;c`;
+  } else if (tempType.value === "fahrenheit") {
+    const Celsiustofahrenheit = (inputValue*(9/5))+32;
+    celsiusElem.innerHTML = `${Celsiustofahrenheit.toFixed(3)} &deg;f`;
+  }
+}
